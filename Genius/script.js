e203bdbf65b11f13f6e720400d6a38844c87b108
Marcello Gomes dos Santos <marcellog.santos@hotmail.com>
2023-04-28 19:31:57 -0300

    Finalizando o Jogo Genius

--- Genius/script.js
+++ Genius/script.js
@@ -18,8 +18,7 @@ function nextSequence() {
     userPattern = [];
     turn = 0;
     level++;
-    document
-        .querySelector('h1').textContent = 'Nível ' + level;
+    document.querySelector('h1').textContent = 'Nível ' + level;
 
     let randomColor =
         buttonColors[Math.floor(Math.random() * buttonColors.length)]
@@ -39,31 +38,44 @@ function nextSequence() {
 
 function flashButton(color) {
     let button = document.querySelector('.' + color)
-    button.style.opacity = '0.5';
-    setTimeout(function () {
-        button.style.opacity = '1'
-    }, 400)
+    if (button !== null) {
+        button.style.opacity = '0.5';
+        setTimeout(function () {
+            button.style.opacity = '1'
+        }, 500)
+    }
 }
 
-document
-    .querySelectorAll('.button')
-    .forEach(function (button) {
-        button.addEventListener('click', function () {
-            if (turn === 1) {
-                let color = button.classList[1];
-                flashButton(color);
-                userPattern.push(color);
-                if (!checkAnswer()) {
-                    alert('Game Over');
-                    gameOver();
-                }
+document.querySelectorAll('.button').forEach(function (button) {
+    button.addEventListener('click', function () {
+        if (turn === 1) {
+            let color = button.classList[1];
+            flashButton(color);
+            userPattern.push(color);
+            if (!checkAnswer()) {
+                alert('Game Over');
+                gameOver();
             }
-        })
+        }
     })
+})
 
 function checkAnswer() {
+    let lastIndex = userPattern.length - 1;
+    if (gamePattern[lastIndex] !== userPattern[lastIndex]) { 
     return false;
 }
+if (userPattern.length === gamePattern.length) {
+    setTimeout(function () {
+        nextSequence()
+    }, 1000);
+} else {
+    turn = 1;
+}
+return true;
+}
+
+
 
 function gameOver() {
     gamePattern = [];
