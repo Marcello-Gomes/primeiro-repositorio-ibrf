dde158ab4352a229f801d7f4f6153b10edea5f01
Marcello Gomes dos Santos <marcellog.santos@hotmail.com>
2023-04-28 22:01:14 -0300

    Atualizando Formulario

---
+++ Formulário/script.js
@@ -0,0 +1,18 @@
+document.getElementById("formulario-cadastro").addEventListener('submit', (event)=> {
+    event.preventDefault();
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value; 
+    const form = { nome, email, senha }
+    //alert(JSON.stringify(form, null, 2))
+
+    fetch(' http://localhost:3000/casdastro', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(form)
+
+    })
+
+})
