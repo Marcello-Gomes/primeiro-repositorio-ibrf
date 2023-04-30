79a755bcbb1c343e54f3bd00435ce3282d57c4f4
Marcello Gomes dos Santos <marcellog.santos@hotmail.com>
2023-04-30 12:31:16 -0300

    finalizando o viaCep

--- ViaCep/script.js
+++ ViaCep/script.js
@@ -4,17 +4,48 @@ function consultarCep() {
     let tabela = document.getElementById("tabela")
 
     axios.get(url).then(response => {
-        tabela.innerHTML = '';
+
         if (response.data.erro) {
             alert("CEP não encontrado")
         }
+        /*
+                tabela.innerHTML = '';
+                let linha = tabela.insertRow();
+                let cepCelula = linha.insertCell(0);
+                let logradouroCelula = linha.insertCell(1);
+                let bairroCelula = linha.insertCell(2);
+                let cidadeCelula = linha.insertCell(3);
+                let estadoCelula = linha.insertCell(4); 
 
-        let linha = tabela.insertRow();
-        let cepCelula = linha.insertCell(0);
-        let logradouroCelula = linha.insertCell(1);
-        let bairroCelula = linha.insertCell(2);
-        let cidadeCelula = linha.insertCell(3);
-        let estadoCelula = linha.insertCell(4);
+                ou
+        
+                */
+
+
+        
+        let linha = document.createElement("tr");
+        let cepCelula = document.createElement("td");
+        let logradouroCelula = document.createElement("td");
+        let bairroCelula = document.createElement("td");
+        let cidadeCelula = document.createElement("td");
+        let estadoCelula = document.createElement("td");
+
+        cepCelula.innerHTML = response.data.cep;
+        logradouroCelula.innerHTML = response.data.logradouro;
+        bairroCelula.innerHTML = response.data.bairro;
+        cidadeCelula.innerHTML = response.data.localidade;
+        estadoCelula.innerHTML = response.data.uf;
+
+        linha.appendChild(cepCelula);
+        linha.appendChild(logradouroCelula);
+        linha.appendChild(bairroCelula);
+        linha.appendChild(cidadeCelula);
+        linha.appendChild(estadoCelula);
+
+        tabela.appendChild(linha);
+
+
+
 
 
 
